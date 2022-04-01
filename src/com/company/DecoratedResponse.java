d9dcdb6097f2bb7778a60b426eafff66876cd8c0
miko7 <mikolaj91048@gmail.com>
2022-04-02 01:46:36 +0200

    Update for PrevInstance. Removed prev instance of Request because it is not valuable for me.

---
+++ src/com/company/DecoratedResponse.java
@@ -0,0 +1,67 @@
+package com.company;
+
+
+import javax.net.ssl.SSLSession;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpHeaders;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Optional;
+
+public class DecoratedResponse<T> implements HttpResponse<T> {
+    public static int decorated_resonse_handler=0;
+    private HttpResponse<T> response;
+    private T body;
+
+    public DecoratedResponse(HttpResponse<T> response){
+        this.response=response;
+        DecoratedResponse.decorated_resonse_handler+=1;
+        this.body = response.body();
+    }
+    
+    public void updateBody(T newBody) {
+        this.body = newBody;
+    }
+
+    @Override
+    public int statusCode() {
+        return response.statusCode();
+    }
+
+    @Override
+    public HttpRequest request() {
+        return response.request();
+    }
+
+    @Override
+    public Optional<HttpResponse<T>> previousResponse() {
+        return response.previousResponse();
+    }
+
+    @Override
+    public HttpHeaders headers() {
+        return response.headers();
+    }
+
+    @Override
+    public T body() {
+        return this.body;
+    }
+
+    @Override
+    public Optional<SSLSession> sslSession() {
+        return response.sslSession();
+    }
+
+    @Override
+    public URI uri() {
+        return response.uri();
+    }
+
+    @Override
+    public HttpClient.Version version() {
+        return response.version();
+    }
+}
+
